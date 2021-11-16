78a5da6e26bbde491ea711bbbfb0c13c3ac476f1
Ephie Oyedoh <ephieoyedoh@Ephies-MacBook-Pro-2.local>
2021-11-16 16:03:28 +0000

    working on erb files

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,13 +3,18 @@ require 'sinatra/activerecord'
 
 class Task < ActiveRecord::Base
   def self.get_all_tasks
-    Task.all
+    Task.all.to_json
   end
 
   def self.get_all_completed_tasks
-    Task.find_by(completed: true)
+    Task.find_all_by(completed: true).limit(10).to_json
   end
   def self.get_all_incomplete_tasks
-    Task.find_by(completed: false)
+    Task.find_all_by(completed: false).limit(10).to_json
   end
 end
+# <% @task.each do |task| %> 
+#   <h2><%=task.task_name%></h2>
+#   <h2><%=task.description%></h2>
+#   <% end %>
+# <h1>all tasks = <%= @task %></h1>

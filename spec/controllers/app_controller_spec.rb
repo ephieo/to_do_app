b15374973ae2721cd399d1f3187ad599dd38aafa
Ephie Oyedoh <ephieoyedoh@Ephies-MacBook-Pro-2.local>
2021-11-20 21:40:01 +0000

    finishsing off controller tests for routes

--- spec/controllers/app_controller_spec.rb
+++ spec/controllers/app_controller_spec.rb
@@ -1,38 +1,44 @@
 require 'spec_helper'
 require 'rspec'
 require 'rack/test'
+require 'json'
 
 require_relative './../../app/controllers/app_controller'
+require_relative './../../app/models/task'
 
-describe TodoApp, :type => :controller  do
+describe TodoApp, :type => :controller do
   include Rack::Test::Methods
-  subject(:todo_app) {TodoApp.new}
-  
+
   def app
-    Sinatra::Application
+    TodoApp.new
   end
 
-context 'GET to /' do 
-    # let(:response) {get '/'}
-    
-    it 'says HOMEPAGE!' do
-      get '/'
-      expect(response).to render_template('index')
-    #    get '/'
-    #    puts last_response 
-    end
-end
- 
+  before do
+    incomplete_task = [{ "id" => 3, "task_name" => "go to the cinema","description" => "at odeon", "completed" => false, "created_at" => "2021-11-16T15:24:21.734Z" }].to_json
+    completed_task = [{ "id" => 4, "task_name" => "go for a walk","description" => "at greenwich park", "completed" => true, "created_at" => "2021-11-16T15:24:21.734Z" }].to_json
+    allow(Task).to receive(:get_all_tasks).and_return(completed_task)
+    allow(Task).to receive(:get_all_incomplete_tasks).and_return(incomplete_task)
+    allow(Task).to receive(:get_all_completed_tasks).and_return(completed_task)
+  end
+
+  it 'says HOMEPAGE!' do
+    get '/'
+    expect(last_response.body).to include('HOMEPAGE')
+  end
 
   it 'shows all tasks' do
-    # let(:response) {get '/all-tasks'}
+    get '/all-tasks'
+    puts last_response.body
+    expect(last_response.body).to include('at greenwich park')
   end
 
-  it 'shows all completed tasks' do
-    get '/complete'
-  end
+    it 'shows all completed tasks' do
+      get '/complete'
+      expect(last_response.body).to include('at greenwich park')
+    end
 
-  it 'shows all incomplete tasks' do
-    get '/incomplete'
-  end
+    it 'shows all incomplete tasks' do
+      get '/incomplete'
+      expect(last_response.body).to include('go to the cinema')
+    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RACK_ENV"] ||= "test"
 require 'simplecov'
 SimpleCov.start
 
-
 require 'bundler'
 Bundler.require(:default, :test)
 

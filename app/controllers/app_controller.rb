809a5bc71e834e95f78c7f1da281fa5955aa9645
Ephie Oyedoh <ephieoyedoh@andrewmacmurray.localdomain>
2021-11-26 12:08:50 +0000

    heroku

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'sinatra/cors'
 require 'sinatra/activerecord'
 require 'json'
 
@@ -13,26 +12,22 @@ class TodoApp < Sinatra::Base
   end
 
   get '/' do
-    erb :'home/index', :layout => :'layouts/layout'
+    "home"
   end
 
   get '/all-tasks' do
    Task.get_all_tasks
   end
   get '/complete' do
-    @tasks = JSON.parse(Task.get_all_completed_tasks)
-    erb :'tasks/all_tasks', :layout => :'layouts/layout'
-    @tasks
+    Task.get_all_completed_tasks
   end
 
   get '/incomplete' do
-    @tasks = JSON.parse(Task.get_all_incomplete_tasks)
-    erb :'tasks/all_tasks', :layout => :'layouts/layout'
-    @tasks
+    Task.get_all_incomplete_tasks
   end
 
   post '/create-task' do
-    @created_task = JSON.parse(Task.create_task(params))
+    Task.create_task(params)
   end
 
   run! if app_file == $0
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require_relative './app/controllers/app_controller'
 
-run TodoApp 
+run Sinatra::Application
